ce66f9f2b04097edc89bcaac8093aa0c374a8640
Mark Seemann <mark@ploeh.dk>
2019-11-28 14:29:45 +0100

    Add aReservation value to test
    This is an example of test data without builders; see
    https://blog.ploeh.dk/2017/09/11/test-data-without-builders This enables
    you to add more fields to the Record type without unnecessarily
    cluttering the tests.

--- RestaurantReservation.Tests/MaîtreDTests.fs
+++ RestaurantReservation.Tests/MaîtreDTests.fs
@@ -4,8 +4,10 @@ open Xunit
 open Swensen.Unquote
 open Ploeh.Kata.MaîtreD
 
+let aReservation = { Quantity = 1 }
+
 [<Theory>]
 [<InlineData( 1,  true)>]
 [<InlineData(13, false)>]
 let ``Boutique restaurant`` quantity expected =
-    expected =! canAccept 12 [] { Quantity = quantity }
+    expected =! canAccept 12 [] { aReservation with Quantity = quantity }
